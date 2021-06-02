cded2d41c2ea799f380c428f0aad2e3cec666874
Pete Goodwin <imekon@users.noreply.github.com>
2021-06-02 08:45:49 +0100

    reduced rock size and added player

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
-onready var rock = preload("res://scenes/Rock.tscn")
+onready var rockScene = preload("res://scenes/Rock.tscn")
+onready var playerScene = preload("res://scenes/Player.tscn")
 
 func _ready():
 	build_rocks()
-	
+
 func build_rocks():
 	for _i in range(0, 5):
-		build_rock(rock)
+		build_rock(rockScene)
+		
+	build_player()
+		
+func build_player():
+	var x = randi() % 1024
+	var y = randi() % 600
+	var player = playerScene.instance()
+	player.position = Vector2(x, y)
+	add_child(player)
+	return player
 	
 func build_rock(rock_scene):
 	var x = randi() % 1024
